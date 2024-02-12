e0e4fd5b6fc14d1b66684ceb511fe68cae8d42c0
chiarraeu <slavitransbg@gmail.com>
2024-02-12 14:54:58 +0200

    modified:   public/css/app.css 	new file:   public/images/chiarra.png 	modified:   resources/views/auth/login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="bg">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-    <title>Login | {{ config('app.name') }}</title>
+    <title>Вход в системата | {{ config('app.name') }}</title>
 
     <!-- Favicon -->
     <link rel="icon" href="{{ asset('images/favicon.png') }}">
@@ -19,7 +19,7 @@
 <div class="container">
     <div class="row mb-3">
         <div class="col-12 d-flex justify-content-center">
-            <img width="200" src="{{ asset('images/logo-dark.png') }}" alt="Logo">
+            <img width="200" src="{{ asset('images/chiarra.png') }}" alt="Logo">
         </div>
     </div>
     <div class="row justify-content-center">
@@ -33,8 +33,8 @@
                 <div class="card-body">
                     <form id="login" method="post" action="{{ url('/login') }}">
                         @csrf
-                        <h1>Login</h1>
-                        <p class="text-muted">Sign In to your account</p>
+                        <h1>Вход в системата</h1>
+                        <p class="text-muted">Въведете вашите данни</p>
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
                                     <span class="input-group-text">
@@ -56,7 +56,7 @@
                             </div>
                             <input id="password" type="password"
                                    class="form-control @error('password') is-invalid @enderror"
-                                   placeholder="Password" name="password">
+                                   placeholder="Парола" name="password">
                             @error('password')
                             <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
@@ -65,16 +65,16 @@
                             <div class="col-4">
                                 <button id="submit" class="btn btn-primary px-4 d-flex align-items-center"
                                         type="submit">
-                                    Login
+                                    Вписване
                                     <div id="spinner" class="spinner-border text-info" role="status"
                                          style="height: 20px;width: 20px;margin-left: 5px;display: none;">
-                                        <span class="sr-only">Loading...</span>
+                                        <span class="sr-only">Вписване...</span>
                                     </div>
                                 </button>
                             </div>
                             <div class="col-8 text-right">
                                 <a class="btn btn-link px-0" href="{{ route('password.request') }}">
-                                    Forgot password?
+                                    Забравена парола?
                                 </a>
                             </div>
                         </div>
@@ -83,8 +83,8 @@
             </div>
 
             <p class="text-center mt-5 lead">
-                Developed By
-                <a href="https://fahimanzam.netlify.app" class="font-weight-bold text-primary">Fahim Anzam Dip</a>
+                Разработен от
+                <a href="https://dev.chiarra.eu" class="font-weight-bold text-primary">V Code Studio</a>
             </p>
         </div>
     </div>
